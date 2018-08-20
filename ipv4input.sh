529689bee7cfe9559ea03eb96cf0b3c86d274b56
Shachaf <x5queal@protonmail.ch>
2018-08-20 14:37:32 +0000

    Create ipv4input.sh

---
+++ ipv4input.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Please enter an IP adderss : " ip
+ipatt="^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+
+if [[ $ip =~ $ipatt ]]; then
+  echo "Valid IP"
+else
+  echo 'Invalid input/IP'
+fi
